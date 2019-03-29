7623046dab04df4ce1424ae42f058fa3ba2376ff
Sadegh Abedi <sadeghabedi@live.com>
2019-03-29 17:10:38 +0430

    create onGenerateRoute

--- lib/luncher.dart
+++ lib/luncher.dart
@@ -11,13 +11,27 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: MainPage(),
       initialRoute: "/splash_screen",
+      onGenerateRoute: _getRoute,
       routes: {
-        "/mainPage" : (context) => new Directionality(textDirection: TextDirection.rtl, child: MainPage()),
-        "/splash_screen" : (context) => new Directionality(textDirection: TextDirection.rtl, child: SplashScreen()),
-
+        "/mainPage": (context) => new Directionality(
+            textDirection: TextDirection.rtl, child: MainPage()),
+        "/splash_screen": (context) => new Directionality(
+            textDirection: TextDirection.rtl, child: SplashScreen()),
       },
+    );
+  }
 
+  Route<dynamic> _getRoute(RouteSettings settings) {
+    if (settings.name != '/splash_screen') {
+      return null;
+    }
+
+    return MaterialPageRoute<void>(
+      settings: settings,
+      builder: (BuildContext context) => SplashScreen(),
+      fullscreenDialog: true,
     );
   }
 }

0f2e6671fd05e36e4c101cf0fdd86b33f7cd1c39
zmmmken <zmmmken@gmail.com>
2019-04-18 13:17:07 +0430

    no message

--- lib/URL.dart
+++ lib/URL.dart
@@ -3,7 +3,7 @@ class URL {
   static const String urlGalley = 'http://$padidehDomain/api/gallery';
   static const String urlNews = 'http://$padidehDomain/api/news';
   static const String urlFame = 'http://$padidehDomain/api/fame';
-  static const String urlcoachList = 'http://$padidehDomain/api/users/coach-list';
+  static const String urlcoachList = 'http://$padidehDomain/api/coach/list';
   static const String urlAboutProgremmer = 'http://$padidehDomain/api/about-us/programmer';
   static const String urlAboutPadideh = 'http://$padidehDomain/api/about-us/information';
   static const String urlSocail = 'http://$padidehDomain/api/support/social/';
@@ -14,7 +14,7 @@ class URL {
   static const String commerceList = 'http://$padidehDomain/api/accounting/commerce-list/';
   static const String commerceCreate = 'http://$padidehDomain/api/accounting/commerce-create/';
   static const String checkList= 'http://$padidehDomain/api/accounting/check-list/';
-  static const String studentList= 'http://$padidehDomain/api/accounting/student-list/';
+  static const String studentList= 'http://$padidehDomain/api/player/list/';
   static const String checkPass= 'http://$padidehDomain/api/accounting/check-pass/';
 //  static const String checkPass= 'http://$padidehDomain/api/accounting/check-pass/';
 

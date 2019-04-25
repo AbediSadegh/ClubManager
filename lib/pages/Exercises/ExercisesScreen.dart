938770498521af7522cb7a3e647596c42f39578b
Sadegh Abedi <sadeghabedi@live.com>
2019-04-25 16:22:01 +0430

    Add API for discription in exercise

--- lib/pages/Exercises/ExercisesScreen.dart
+++ lib/pages/Exercises/ExercisesScreen.dart
@@ -157,20 +157,18 @@ class _EcerciseListState extends State<EcerciseList> {
       key: _listkey,
       itemBuilder: (context, index, animation) {
         Exercise t = Exercise(
-          animation: animation,
-          color: exerciseEntity[index].color == 0
-              ? Colors.yellow
-              : exerciseEntity[index].color == 1
-                  ? Colors.blue
-                  : exerciseEntity[index].color == 2
-                      ? Colors.red
-                      : Colors.green,
-          title: exerciseEntity[index].title,
-          time: exerciseEntity[index].time,
-          subtitle: exerciseEntity[index].subtitle,
-          description:
-              '''لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.''',
-        );
+            animation: animation,
+            color: exerciseEntity[index].color == 0
+                ? Colors.yellow
+                : exerciseEntity[index].color == 1
+                    ? Colors.blue
+                    : exerciseEntity[index].color == 2
+                        ? Colors.red
+                        : Colors.green,
+            title: exerciseEntity[index].title,
+            time: exerciseEntity[index].time,
+            subtitle: exerciseEntity[index].subtitle,
+            description: exerciseEntity[index].description);
         return t;
       },
     );
